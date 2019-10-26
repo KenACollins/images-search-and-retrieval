5cfdfd29b5b40f7bf8c402d45da5c56db98be301
Ken A Collins <Ken@KenACollins.com>
2019-10-26 13:55:00 -0400

    Test API with hardcoded search term.

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // API key is passed in request with searh term in App.js
 export default axios.create({
-    baseURL: 'api.giphy.com/v1/gifs/search'
+    baseURL: 'http://api.giphy.com'
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import giphy from '../api/giphy';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
+
 const keys = require('../config/keys');
 
 class App extends React.Component {
+    state = { images: [] };
+
+    onSearchSubmit = async term => {
+        const response = await giphy.get('/v1/gifs/search', {
+            params: {
+                q: term,
+                api_key: keys.giphyApiKey
+            }
+        });
+        
+        this.setState({ images: response.data.data, response });
+        console.log(this.state.images);
+    };
+
+    componentDidMount() {
+        this.onSearchSubmit('cars');
+    }
+
     render() {
         return (
             <div>
