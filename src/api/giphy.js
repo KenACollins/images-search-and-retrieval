4e7ff8e336c7464886424317af84991190e70e71
Ken A Collins <Ken@KenACollins.com>
2019-10-27 19:34:28 -0400

    Changed Giphy API URL from HTTP to HTTPS as required by Heroku production server app which runs from HTTPS URL.

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -3,14 +3,9 @@ import axios from 'axios';
 const keys = require('../config/keys');
 const MAX_RESULTS_PER_PAGE = 50; // We only want to retrieve 50 images per API call.
 
-// API key is passed in request with searh term in App.js
-export const giphy = axios.create({
-    baseURL: 'http://api.giphy.com'
-});
-
 const getImages = async (searchTerm = '', currentOffset = 0) => {
     try {
-        const response = await axios.create({ baseURL: 'http://api.giphy.com' }).get('/v1/gifs/search', {
+        const response = await axios.create({ baseURL: 'https://api.giphy.com' }).get('/v1/gifs/search', {
             params: {
                 q: searchTerm,
                 api_key: keys.giphyApiKey,
@@ -18,6 +13,7 @@ const getImages = async (searchTerm = '', currentOffset = 0) => {
                 offset: currentOffset
             }
         });
+        console.log("Using https URL");
         return response;
     }
     catch (err) {
