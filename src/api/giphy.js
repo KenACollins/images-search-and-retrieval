2298b876c085d7f898d5b16e2425dbe9262cb15e
Ken A Collins <Ken@KenACollins.com>
2019-10-27 17:16:51 -0400

    API calls moved to separate module.

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -1,6 +1,28 @@
 import axios from 'axios';
 
+const keys = require('../config/keys');
+const MAX_RESULTS_PER_PAGE = 50; // We only want to retrieve 50 images per API call.
+
 // API key is passed in request with searh term in App.js
-export default axios.create({
+export const giphy = axios.create({
     baseURL: 'http://api.giphy.com'
 });
+
+const getImages = async (searchTerm = '', currentOffset = 0) => {
+    try {
+        const response = await axios.create({ baseURL: 'http://api.giphy.com' }).get('/v1/gifs/search', {
+            params: {
+                q: searchTerm,
+                api_key: keys.giphyApiKey,
+                limit: MAX_RESULTS_PER_PAGE,
+                offset: currentOffset
+            }
+        });
+        return response;
+    }
+    catch (err) {
+        return err;
+    }
+};
+
+export default getImages;
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import giphy from '../api/giphy';
+import getImages from '../api/giphy';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
-
-const keys = require('../config/keys');
 
 class App extends React.Component {
     state = { previousSearchTerm: '', images: [], totalCount: 0, noResultsCode: 0 };
     maxResultsPerPage = 50; // We only want to retrieve 50 images per API call.
     currentOffset = 0;      // Akin to page number, index position starting point for next batch of results.
 
+    /**
+     * This function is called when the user types a search term into the input field and presses Enter. It replaces any history 
+     * of previously retrieved images with the first batch of images meeting the new criteria. 
+     * 
+     * If user has already gotten results for that search term, and perhaps scrolled down to obtain multiple batches of results, 
+     * pressing Enter will reset the screen to the first batch of images.
+     */
     onSearchSubmit = async term => {
         term = term.trim(); // Remove any extraneous white space characters in beginning or end of search term.
 
@@ -28,43 +33,46 @@ class App extends React.Component {
         this.setState({ previousSearchTerm: '', images: [], totalCount: 0, noResultsCode: 0 });
         this.currentOffset = 0;
 
-        const response = await giphy.get('/v1/gifs/search', {
-            params: {
-                q: term,
-                api_key: keys.giphyApiKey,
-                limit: this.maxResultsPerPage,
-                offset: this.currentOffset
-            }
-        });
+        const response = await getImages(term, this.currentOffset);
         
-        // For new searches, replace images array with data returned in this API response.
-        const total_count = (response.data.pagination && response.data.pagination.total_count) ? response.data.pagination.total_count : 0;
-        if (total_count === 0) { this.setState({ noResultsCode: 2}); }
-        this.setState({ previousSearchTerm: term, images: response.data.data, totalCount: total_count });
+        if (response && response.data) {
+            // Prevent code from bombing when pagination object is missing.
+            const total_count = (response.data.pagination && response.data.pagination.total_count) ? response.data.pagination.total_count : 0;
+            if (total_count === 0) { this.setState({ noResultsCode: 2}); }
+            
+            // For new searches, replace images array with data returned in this API response.
+            this.setState({ previousSearchTerm: term, images: response.data.data, totalCount: total_count });
+        }
     };
-
+    
+    /**
+     * This function is called whenever the user scrolls down to the bottom of a screen full of images, causing another API call to retrieve
+     * an additional batch of images. If the user reaches the end of the result set, a warning message indicating 'no MORE results' will appear.
+     */
     addMoreImages = async () => {
         // If moving the offset to the next position goes beyond the total number of results, alert user there are no more results. Avoid needless API call.
         if (this.currentOffset + this.maxResultsPerPage + 1 > this.state.totalCount) {
             this.setState({ noResultsCode: 3 });
             return;
         }
+
+        // Okay, it is safe to increment offset for next API call.
         this.currentOffset += this.maxResultsPerPage + 1;
         
-        const response = await giphy.get('/v1/gifs/search', {
-            params: {
-                q: this.state.previousSearchTerm,
-                api_key: keys.giphyApiKey,
-                limit: this.maxResultsPerPage,
-                offset: this.currentOffset
-            }
-        });
-        
-        // For additional searches of same search term, concatenate images array with data returned in this API response.
-        const total_count = (response.data.pagination && response.data.pagination.total_count) ? response.data.pagination.total_count : 0;
-        this.setState({ images: [...this.state.images, ...response.data.data], totalCount: total_count });
+        const response = await getImages(this.state.previousSearchTerm, this.currentOffset);
+               
+        if (response && response.data) {
+            // For subsequent searches, total_count should have the same non-zero value as it did in first search.
+            const total_count = (response.data.pagination && response.data.pagination.total_count) ? response.data.pagination.total_count : 0;
+
+            // For additional searches of same search term, concatenate images array with data returned in this API response.
+            this.setState({ images: [...this.state.images, ...response.data.data], totalCount: total_count });
+        }
     };
 
+    /**
+     * This function gracefully handles situations where there is a lack of results by displaying an appropriate message on the screen.
+     */
     showNoResultsMessage = () => {
         switch(this.state.noResultsCode) {
             case 1: return <div>{`Please type a search term into the field above and press Enter.`}</div>;
@@ -74,13 +82,20 @@ class App extends React.Component {
         }
     };
 
-    // Format large numbers with thousands separator. Example: 12345 => 12,345
+    /**
+     * This utility function formats large numbers with thousands separator. Example: 12345 => 12,345
+     */
     formatNumber = num => {
         return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
     };
 
     componentDidMount() {
-        // Must set window.onscroll to an arrow function or else 'this' will not be resolved.
+        /**
+         * Detect if user has scrolled to the bottom of the screen so that another batch of images matching the search criteria
+         * can automatically be retrieved without the need for the user to click a button or do anything special.
+         * 
+         * FYI, window.onscroll must be set to an arrow function or else 'this' will not be resolved. 
+         */
         window.onscroll = () => {
             if (!this.state || this.state.previousSearchTerm === '') { return; }
     
