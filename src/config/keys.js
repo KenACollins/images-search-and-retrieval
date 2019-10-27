d4299511b55f4afd3fe452212fe926c7e47d61a4
Ken A Collins <Ken@KenACollins.com>
2019-10-27 19:06:05 -0400

    Testing for prod.

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,5 @@
 // key.js - Figure out which set of credentials to return based on environment (dev or prod).
+console.log('Environment: ' + process.env.NODE_ENV); // testing
 if (process.env.NODE_ENV === 'production') {
     // We are in production, return prod set of keys.
     module.exports = require('./prod');
